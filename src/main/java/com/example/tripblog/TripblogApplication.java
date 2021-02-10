f14f1c4d328fa149fa7d91ac159d526bf86adc7c
Sang-un <dltkddns79@gmail.com>
2021-02-10 16:08:15 +0900

    admin spring

---
+++ src/main/java/com/example/tripblog/TripblogApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tripblog/TripblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
